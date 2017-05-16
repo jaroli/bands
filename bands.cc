702872282d4d8f8f0b0a800c6dbe60a058408afc
jaroli <karol.jarolimek@gmail.com>
2017-05-16 12:10:47 -0500

    removed header

--- bands.cc
+++ bands.cc
@@ -1,14 +1,3 @@
-/*
- * karol jarolimek
-  program bands
-  -plot band structure
-  -input: OUTCAR, KPOINTS, EIGENVAL
-
-  -use scale=1.0 in your POSCAR to avoid confusion
- i changed this
-*/
-
-
 #include <iostream>
 #include <fstream>
 #include <string>
